58c83c27e8270f1c767eabfffea83a9b151bcb77
yamenmarquez <yamenmarquez@gmail.com>
2019-05-05 00:12:23 -0400

    Cambio de nombre del archivo ze.py a run.py

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-# ze.py
+# run.py
 import os
 
 from app import app
@@ -6,5 +6,5 @@ from app import app
 if __name__ == '__main__':
     app.run('0.0.0.0', debug=True)
 
-#os.popen("export FLASK_APP=ze.py")
-#os.popen("flask run")
+os.popen("export FLASK_APP=run.py")
+os.popen("flask run")
